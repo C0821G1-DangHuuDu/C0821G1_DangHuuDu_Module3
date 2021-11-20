bcbcaa169176344a52c3b68f17a737c87f7864eb
C0821G1-DangHuuDu <huud90@gmail.com>
2021-11-20 21:22:21 +0700

    case study

--- furama_resort_manager/cau1_nhap_thong_tin.sql
+++ furama_resort_manager/cau1_nhap_thong_tin.sql
@@ -47,6 +47,7 @@ insert into khach_hang value (3,2,'David','1988/01/15',1, '205333111','090533311
 insert into khach_hang value (4,2,'Mary','1981/03/17',0, '205222333','0905222333','mary@gmail.com', 'CAN');
 insert into khach_hang value (5,2,'Mary','1985/02/19',0, '205222444','0905222333','mary1@gmail.com', 'ENG');
 insert into khach_hang value (6,2,'David','1988/01/15',1, '205333111','0905333111','david@gmail.com', 'USA');
+update khach_hang set ho_ten = 'John', so_cmnd = '205321123', so_dien_thoai = '0905321123', email = 'john@gmail.com', dia_chi = 'FRA' where ma_khach_hang=6;
 
 -- Nhập dữ liệu vào bảng kiểu thuê:
 insert into kieu_thue value (1,'theo giờ',100);
@@ -76,6 +77,9 @@ insert into hop_dong value (3,'2018/07/17 14:00:00','2018/07/18 12:00:00', 100, 
 insert into hop_dong value (4,"2020/04/03 14:00:00","2020/04/04 12:00:00",20,15.00,4,2,4);
 insert into hop_dong value (5,"2020/07/20 14:00:00","2020/07/20 22:00:00",15,15.00,4,1,5);
 insert into hop_dong value (6,"2020/05/20 14:00:00","2020/06/20 12:00:00",20,100,5,4,6);
+insert into hop_dong value (7,"2019/10/20 14:00:00","2020/11/20 12:00:00",50,100,5,2,6);
+insert into hop_dong value (8,"2015/10/20 14:00:00","2015/11/20 12:00:00",50,100,5,6,3);
+insert into hop_dong value (9,"2015/10/20 14:00:00","2015/11/20 12:00:00",50,100,3,2,3);
 
 -- Nhập dữ liệu vào bảng dịch vụ đi kèm:
 insert into dich_vu_di_kem value (1,'massage', 10, 1, 'sẵn sàng');
@@ -91,6 +95,7 @@ insert into hop_dong_chi_tiet value (3,5,4, 3);
 insert into hop_dong_chi_tiet value (4,6,2, 1);
 insert into hop_dong_chi_tiet value (5,6,5, 2);
 insert into hop_dong_chi_tiet value (6,6,4, 4);
+insert into hop_dong_chi_tiet value (7,7,4, 6);
 
 
 
@@ -145,6 +150,18 @@ where year(hd.ngay_lam_hop_dong) between 2019 and year(curdate()));
 select distinct ho_ten
 from khach_hang;
 
+-- Cách 2: 
+select  ho_ten
+from khach_hang
+group by ho_ten;
+
+-- Cách 3:
+select  ho_ten
+from khach_hang
+union
+ select  ho_ten
+from khach_hang;
+
 -- Task 9:
 select ldv.ten_loai_dịch_vu,dv.ten_dich_vu,hd.ngay_lam_hop_dong,hd.tong_tien,month(hd.ngay_lam_hop_dong) as 'Tháng',(hd.tong_tien) as 'Tổng doanh thu'
 from hop_dong hd
@@ -161,9 +178,115 @@ inner join dich_vu_di_kem dvdk on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_ke
 group by hd.ma_hop_dong;
 
 -- Task 11:
+select dvdk.ma_dich_vu_di_kem,dvdk.ten_dich_vu_di_kem,dvdk.gia,kh.ho_ten,kh.dia_chi,lk.ten_loai_khach
+from khach_hang kh
+inner join loai_khach lk on kh.ma_loai_khach = lk.ma_loai_khach
+inner join hop_dong hd on kh.ma_khach_hang = hd.ma_khach_hang
+inner join hop_dong_chi_tiet hdct on hd.ma_hop_dong = hdct.ma_hop_dong
+inner join dich_vu_di_kem dvdk on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+where lk.ten_loai_khach = 'Diamond' and (kh.dia_chi='Đà Nẵng' or kh.dia_chi='Quảng Nam');
 
+-- Task 12:
+select hd.ma_hop_dong,nv.ho_va_ten,kh.ho_ten,kh.so_dien_thoai,kh.dia_chi,lk.ten_loai_khach,dvdk.ten_dich_vu_di_kem,dvdk.gia,sum(hdct.so_luong) as 'so_luong_dich_vu_di_kem',sum(hd.tien_dat_coc) as tien_dat_coc
+from khach_hang kh
+inner join loai_khach lk on kh.ma_loai_khach = lk.ma_loai_khach
+inner join hop_dong hd on kh.ma_khach_hang = hd.ma_khach_hang
+inner join nhan_vien nv on hd.ma_nhan_vien = nv.ma_nhan_vien
+inner join hop_dong_chi_tiet hdct on hd.ma_hop_dong = hdct.ma_hop_dong
+inner join dich_vu_di_kem dvdk on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+where year(hd.ngay_lam_hop_dong)=2019 and month(hd.ngay_lam_hop_dong) not in (1,2,3,4,5,6)
+group by hd.ma_hop_dong;
 
+-- Task 13:
+create view Sum_so_luong_dich_vu_di_kem
+as
+select hd.ma_hop_dong,kh.ho_ten,kh.so_dien_thoai,kh.dia_chi,lk.ten_loai_khach,sum(hdct.so_luong) as so_luong_dich_vu_di_kem
+from hop_dong hd
+inner join khach_hang kh on kh.ma_khach_hang = hd.ma_khach_hang
+inner join loai_khach lk on kh.ma_loai_khach = lk.ma_loai_khach
+inner join hop_dong_chi_tiet hdct on hd.ma_hop_dong = hdct.ma_hop_dong
+inner join dich_vu_di_kem dvdk on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+group by kh.ho_ten;
 
+select *
+from Sum_so_luong_dich_vu_di_kem
+where so_luong_dich_vu_di_kem=(select max(so_luong_dich_vu_di_kem) from Sum_so_luong_dich_vu_di_kem);
+
+-- Task 14: 
+select hd.ma_hop_dong,ldv.ten_loai_dịch_vu,dvdk.ten_dich_vu_di_kem,count(hdct.so_luong) as so_lan_su_dung
+from hop_dong hd
+inner join dich_vu dv on hd.ma_dich_vu = dv.ma_dich_vu
+inner join loai_dich_vu ldv on ldv.ma_loai_dich_vu = dv.ma_loai_dich_vu
+inner join hop_dong_chi_tiet hdct on hd.ma_hop_dong = hdct.ma_hop_dong
+inner join dich_vu_di_kem dvdk on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+group by dvdk.ma_dich_vu_di_kem;
+
+-- Task 15:
+select nv.ma_nhan_vien,nv.ho_va_ten,td.ten_trinh_do,bp.ten_bo_phan,nv.so_dien_thoai,nv.dia_chi,count(hd.ma_hop_dong) as So_luong_hop_dong
+from nhan_vien nv
+inner join trinh_do td on nv.ma_trinh_do = td.ma_trinh_do
+inner join bo_phan bp on nv.ma_bo_phan = bp.ma_bo_phan
+inner join hop_dong hd on nv.ma_nhan_vien = hd.ma_nhan_vien
+group by nv.ma_nhan_vien
+having So_luong_hop_dong<=3;
+
+-- Task 16:
+create view Danh_sach_nhan_vien_va_hop_dong
+as
+select nv.ma_nhan_vien,nv.ho_va_ten,nv.so_dien_thoai,nv.dia_chi,count(hd.ma_hop_dong) as So_luong_hop_dong
+from nhan_vien nv
+left join trinh_do td on nv.ma_trinh_do = td.ma_trinh_do
+left join bo_phan bp on nv.ma_bo_phan = bp.ma_bo_phan
+left join hop_dong hd on nv.ma_nhan_vien = hd.ma_nhan_vien
+group by nv.ma_nhan_vien;
+
+delete from nhan_vien where ma_nhan_vien not in (select ma_nhan_vien from Danh_sach_nhan_vien_va_hop_dong where So_luong_hop_dong<>0);
+
+-- Task 17:
+create view Tong_tien_thanh_toan_nam_2019
+as
+select kh.ma_khach_hang,kh.ho_ten,kh.so_dien_thoai,kh.dia_chi,lk.ten_loai_khach,sum(hd.tong_tien) as Tong_tien_thanh_toan
+from hop_dong hd
+inner join khach_hang kh on kh.ma_khach_hang = hd.ma_khach_hang
+inner join loai_khach lk on kh.ma_loai_khach = lk.ma_loai_khach
+where year(hd.ngay_lam_hop_dong)=2019 and kh.ma_loai_khach =2
+group by kh.ma_khach_hang;
+
+SET SQL_SAFE_UPDATES=0;
+update khach_hang set ma_loai_khach = 1 where ma_khach_hang in (select ma_khach_hang from Tong_tien_thanh_toan_nam_2019 where Tong_tien_thanh_toan>200);
+SET SQL_SAFE_UPDATES=1;
+
+-- Task 18:
+create view danh_sach_hop_dong_theo_khach_hang
+as
+select kh.ma_khach_hang,kh.ho_ten,kh.so_dien_thoai,kh.dia_chi,sum(year(hd.ngay_lam_hop_dong)) as nam_hop_dong
+from hop_dong hd
+inner join khach_hang kh on kh.ma_khach_hang = hd.ma_khach_hang
+group by kh.ma_khach_hang;
+
+SET SQL_SAFE_UPDATES=0;
+delete from hop_dong where ma_khach_hang in (select ma_khach_hang from danh_sach_hop_dong_theo_khach_hang where nam_hop_dong <2016);
+delete from khach_hang where ma_khach_hang in (select ma_khach_hang from danh_sach_hop_dong_theo_khach_hang where nam_hop_dong <2016);
+SET SQL_SAFE_UPDATES=1;
+
+-- Task 19:
+create view so_lan_su_dung_dvdk
+as
+select dvdk.ma_dich_vu_di_kem,dvdk.ten_dich_vu_di_kem,dvdk.gia,sum(hdct.so_luong) as so_lan_su_dung
+from dich_vu_di_kem dvdk
+inner join hop_dong_chi_tiet hdct on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+group by dvdk.ma_dich_vu_di_kem;
+
+SET SQL_SAFE_UPDATES=0;
+update dich_vu_di_kem set gia = gia*2 where ma_dich_vu_di_kem in (select ma_dich_vu_di_kem from so_lan_su_dung_dvdk where so_lan_su_dung >10);
+SET SQL_SAFE_UPDATES=1;
+
+-- Task 20:
+select ma_nhan_vien as id,ho_va_ten,ngay_sinh,so_dien_thoai,email,dia_chi
+from nhan_vien
+union
+select ma_khach_hang,ho_ten,ngay_sinh,so_dien_thoai,email,dia_chi
+from khach_hang;
 
 
 
